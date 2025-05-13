e02230b3ee05af18641062741fd4e1b06bcf74ef
nafnix <uwu@nafnix.com>
2025-05-13 14:27:54 +0800

    feat: 更新 `url_path`, 改为直接子类化 `str`

--- src/fastapi_exts/url_path.py
+++ src/fastapi_exts/url_path.py
@@ -1,30 +1,43 @@
 import re
-from collections import UserString
-from typing import Self
+from typing import Self, overload
+
+from typing_extensions import Buffer
 
 
-class URLPath(UserString):
-    _replace_pattern = re.compile(r"^(?:\/)+|(?:\/)+$")
+_replace_pattern = re.compile(r"^(?:\/)+|(?:\/)+$")
 
-    def _validation(self, value: str) -> str:
-        return self._replace_pattern.sub("", value)
 
-    def __init__(self, seq: str | int | None = None) -> None:
-        if seq is None:
-            seq = ""
+def _transform(value):
+    return _replace_pattern.sub("", value)
 
-        seq = self._validation(seq) if isinstance(seq, str) else f"{seq}"
-        seq = f"/{seq}"
 
-        super().__init__(seq)
+class URLPath(str):
+    __slots__ = ()
+
+    @staticmethod
+    def _initial(seq: str):
+        return f"/{_transform(seq)}"
+
+    @overload
+    def __new__(cls, object: object = ...): ...
+    @overload
+    def __new__(
+        cls,
+        object: Buffer,
+        encoding: str = ...,
+        errors: str = ...,
+    ): ...
+    def __new__(cls, object: object | Buffer = ..., *args, **kwds) -> Self:  # noqa: A002
+        string = str(object, *args, **kwds)
+        return super().__new__(cls, cls._initial(string))
 
     def __truediv__(self, other: Self | str | int):
-        args: list[str] = [self.data]
+        args: list[str] = [self]
         if not isinstance(other, str):
             args.append(str(other))
         else:
             args.append(other)
 
-        path = "/".join([self._validation(arg) for arg in args])
+        path = "/".join([_transform(arg) for arg in args])
 
         return URLPath(path)
