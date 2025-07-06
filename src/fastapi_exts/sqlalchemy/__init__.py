2cd680f513bb7bb348bfd8c365bce3c40fa04b3a
nafnix <uwu@nafnix.com>
2025-07-06 23:20:06 +0800

    feat: sqlalchemy 添加混合模型

--- src/fastapi_exts/sqlalchemy/__init__.py
+++ src/fastapi_exts/sqlalchemy/__init__.py
@@ -1,8 +1,12 @@
-from .pagination import page
+from .mixins import AuditMixin, IDBase
+from .pagination import api_page, page
 from .session import create_engine_dependency, create_session_dependency
 
 
 __all__ = [
+    "AuditMixin",
+    "IDBase",
+    "api_page",
     "create_engine_dependency",
     "create_session_dependency",
     "page",
